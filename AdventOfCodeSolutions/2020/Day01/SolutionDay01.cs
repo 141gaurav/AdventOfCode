b374547bba04a784a4b5ce6a064bad9a355f72f4
141gaurav <19299987+141gaurav@users.noreply.github.com>
2021-07-25 16:57:12 +0530

    Pushing Advent of code Day 01 solution

---
+++ AdventOfCodeSolutions/2020/Day01/SolutionDay01.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeSolutions._2020.Day01
+{
+    public class SolutionDay01
+    {
+        public int ReturnExpenseReport(int[] input,int expenseSum = 2020)
+        {
+            int expense = 0;
+            int totalExp = 0;
+
+            for (int i = 0; i < input.Count(); i++)
+            {
+                expense = expenseSum - input[i];
+
+                for (int k = 0; k < input.Count(); k++)
+                {
+                    if (expense == input[k])
+                        totalExp = expense * input[i];
+                }
+            }
+            return totalExp;
+        }
+
+        public int ReturnProductOfThreeEntries(int[] input, int expenseSum = 2020)
+        {
+            int totalExp = 0;
+
+            for (int i = 0; i < input.Count(); i++)
+            {
+                for (int j = i + 1; j < input.Count(); j++)
+                {
+                    if (input[i] + input[j] < expenseSum)
+                    {
+                        for (int k = i + 2; k < input.Count(); k++)
+                        {
+                            int sumThreeNumbers = input[i] + input[j] + input[k];
+                            if (sumThreeNumbers==expenseSum)
+                            {
+                                totalExp = input[i] * input[j] * input[k];
+                            }
+                        }
+                    }
+                }
+            }
+            return totalExp;
+        }
+    }
+}
---
+++ AdventOfCodeSolutions/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCodeSolutions._2020.Day01;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCodeSolutions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day 01 - Part 01 & 02
+            int[] inputday01 = File.ReadLines(@"F:\AOC\AdventOfCode\AdventOfCodeSolutions\2020\Day01\input.txt")
+                .Select(nums => Convert.ToInt32(nums)).ToArray();
+
+            SolutionDay01 solutionDay01 = new SolutionDay01();
+            Console.WriteLine("Day 01 Part 01 AOC answer is :" + solutionDay01.ReturnExpenseReport(inputday01));
+            Console.WriteLine("Day 01 Part 02 AOC answer is :" + solutionDay01.ReturnProductOfThreeEntries(inputday01));
+        }
+    }
+}
